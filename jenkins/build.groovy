dc611e5082d0c608dfdad6b8578a8835741c1668
moshe.even <moshe.even@imperva.com>
2022-03-24 22:16:34 +0200

    wip

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -1,5 +1,6 @@
 node(''){
     properties([pipelineTriggers([githubPush()])])
+
     
     stage('git clone'){
         git branch: 'moshe/7boom', url: 'https://github.com/mosheeven/gradle-hello-world.git'
